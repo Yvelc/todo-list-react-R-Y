303d8c5a4baabc4e8446887e884b382c7d38a808
Yvelc Fonseca <yvelcfonseca@gmail.com>
2019-08-13 17:25:38 -0400

    change the icon of the list

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
         >
           Learn React
         </a>
-        <ul className="list-decimal"> {/* colocar "App-textDec" y descomentar la clase en el Amm.css para hacer lo mismo con Css aca esta con tailwind */}
+        <ul className="list-disc"> {/* colocar "App-textDec" y descomentar la clase en el Amm.css para hacer lo mismo con Css aca esta con tailwind */}
             <li>1</li>
             <li>2</li>
             <li>3</li>
