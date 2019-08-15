31f3344de869b6d9e0abdcb73181a054f41a121a
Yvelc Fonseca <yvelcfonseca@gmail.com>
2019-08-15 14:25:06 -0400

    border radius

--- src/components/Tlist.js
+++ src/components/Tlist.js
@@ -2,7 +2,7 @@ import React from "react"
 
 function TList(){
     return (
-        <main className= "bg-indigo-200 text-indigo-500 justify-start w-3/4 border-b-2">
+        <main className= "bg-indigo-200 text-indigo-500 justify-start w-3/4 border-b-2 rounded">
       
            <input type="checkbox" />
            <p>Task </p>
