dc18580630c4d98021b20a067002ec419e337bfd
Yvelc Fonseca <yvelcfonseca@gmail.com>
2019-08-15 17:17:36 -0400

    move checkbox

--- src/components/Tlist.js
+++ src/components/Tlist.js
@@ -2,9 +2,9 @@ import React from "react"
 
 function TList(){
     return (
-        <main className= "bg-indigo-200 text-indigo-500 justify-start w-3/4 border-b-2 rounded">
-      
+        <main className= "bg-indigo-200 flex text-indigo-500 justify-center w-3/4 border-b-2 rounded">
            <input type="checkbox" />
+           <input type="text" className=" flex justify-start items-center pl-1 w-3/5 bg-indigo-100" />
            <p>Task </p>
        
         </main>
