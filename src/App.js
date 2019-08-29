f553b2bcc740c5d3b4e50bdd539d7a8d4ffebb12
Yvelc Fonseca <yvelcfonseca@gmail.com>
2019-08-29 17:28:14 -0400

    ContactCard

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Header from './components/Header'
 import MainContent from './components/MainContent'
 import Footer from "./components/Footer"
 import Tlist from "./components/Tlist"
+import ContactCard from "./components/conactCard"
 //import TodoItem from "./components/TodoItem"
 
 function App() {
@@ -37,6 +38,26 @@ function App() {
       <Tlist/>
       <Tlist/>
       <Tlist/>
+
+
+            <ContactCard 
+                contact={{name: "Mr. Whiskerson", imgUrl: "http://placekitten.com/300/200", phone: "(212) 555-1234", email: "mr.whiskaz@catnap.meow"}}
+            />
+            
+            <ContactCard 
+                contact={{name: "Fluffykins", imgUrl: "http://placekitten.com/400/200", phone: "(212) 555-2345", email: "fluff@me.com"}}
+            />
+            
+            <ContactCard
+                contact={{name: "Destroyer", imgUrl: "http://placekitten.com/400/300", phone: "(212) 555-3456", email: "ofworlds@yahoo.com"}}
+            />
+            
+            <ContactCard 
+                contact={{name: "Felix", imgUrl: "http://placekitten.com/200/100", phone: "(212) 555-4567", email: "thecat@hotmail.com"}}
+            />
+            
+      
+      
 
       <Footer />
     
